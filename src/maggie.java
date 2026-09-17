a88eb51e21b58408d643792f663becb1a3aaea3b
suryam tomar <tomarsuryam11@gmail.com>
2026-09-17 19:42:33 +0530

    "loops in java"

--- src/maggie.java
+++ src/maggie.java
@@ -1,5 +1,13 @@
 public class maggie {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+ boolean a=true;
+ boolean b=false;
+
+        System.out.println(a&&b);
+        System.out.println(a||b);
+        System.out.println(!a);
+
+
+
     }
 }
