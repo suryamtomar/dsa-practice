731020dce2ba9c436b868f262fcb0cb7f9b87563
suryam tomar <tomarsuryam11@gmail.com>
2026-09-18 23:13:35 +0530

    "Pattern-2:Staircase Shape Pattern"

--- src/patternPrintingPart2.java
+++ src/patternPrintingPart2.java
@@ -1,22 +1,41 @@
 public class patternPrintingPart2 {
     public static void main(String[] args) {
         //Pattern-1:Hollow Rectangular Pattern:-
+//        int n=5;
+//        for(int row=1;row<=n;row++){
+//            for(int col=1;col<=6;col++){
+//                 if(row==1 ||row==n){
+//                     System.out.print("*");
+//                 }else{
+//                     if(col==1){
+//                         System.out.print("*");
+//                     }else if(col==6){
+//                         System.out.print("*");
+//                     }else {
+//                         System.out.print(" ");
+//                     }
+//                 }
+//            }
+//            System.out.println();
+//        }
+        //Pattern-2:Staircase Shape Pattern:-
         int n=5;
         for(int row=1;row<=n;row++){
-            for(int col=1;col<=6;col++){
-                 if(row==1 ||row==n){
-                     System.out.print("*");
-                 }else{
-                     if(col==1){
-                         System.out.print("*");
-                     }else if(col==6){
-                         System.out.print("*");
-                     }else {
-                         System.out.print(" ");
-                     }
-                 }
+
+                if (row == 1) {
+                    System.out.println("* ");
+                } else {
+                    if (row == 2) {
+                        System.out.println("* * ");
+                    } else if (row == 3) {
+                        System.out.println("*   * ");
+                    } else if (row == 4) {
+                        System.out.println("*     * ");
+                    } else {
+                        System.out.println("* * * * *");
+                    }
+                }
             }
-            System.out.println();
         }
 
 
@@ -30,6 +49,5 @@ public class patternPrintingPart2 {
 
 
 
-
     }
-}
+
