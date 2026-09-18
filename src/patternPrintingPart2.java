cdde567adff7ddeb67fe4012009540a66e1ace6e
suryam tomar <tomarsuryam11@gmail.com>
2026-09-18 23:37:10 +0530

    "Pattern-2:Another Approach To Solve Staircase Shape Pattern"

--- src/patternPrintingPart2.java
+++ src/patternPrintingPart2.java
@@ -19,35 +19,45 @@ public class patternPrintingPart2 {
 //            System.out.println();
 //        }
         //Pattern-2:Staircase Shape Pattern:-
-        int n=5;
-        for(int row=1;row<=n;row++){
-
-                if (row == 1) {
-                    System.out.println("* ");
-                } else {
-                    if (row == 2) {
-                        System.out.println("* * ");
-                    } else if (row == 3) {
-                        System.out.println("*   * ");
-                    } else if (row == 4) {
-                        System.out.println("*     * ");
-                    } else {
-                        System.out.println("* * * * *");
-                    }
+//        int n=5;
+//        for(int row=1;row<=n;row++){
+//
+//                if (row == 1) {
+//                    System.out.println("* ");
+//                } else {
+//                    if (row == 2) {
+//                        System.out.println("* * ");
+//                    } else if (row == 3) {
+//                        System.out.println("*   * ");
+//                    } else if (row == 4) {
+//                        System.out.println("*     * ");
+//                    } else {
+//                        System.out.println("* * * * *");
+//                    }
+//                }
+//            }
+//        }
+        //Pattern-2:Another Approach To Solve Staircase Shape Pattern:-
+        int n = 10;
+        for (int row = 1; row <= n; row++) {
+            if(row==1||row==2||row==n){
+                for(int col = 1; col <= row; col++){
+                    System.out.print("* ");
                 }
+            }else{
+//                first star
+                System.out.print("* ");
+//                middle star
+            for(int col = 1; col <= row-2; col++){
+                System.out.print("  ");
             }
+            //            last star
+            System.out.print("* ");
+            }
+//           Move to next row
+            System.out.println();
+
         }
-
-
-
-
-
-
-
-
-
-
-
-
+      }
     }
 
