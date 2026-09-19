19211d0c381dc580fe667e8977826d621c7abf26
suryam tomar <tomarsuryam11@gmail.com>
2026-09-19 18:37:50 +0530

    "1st example:for printing the table of 2"

---
+++ src/methods.java
@@ -0,0 +1,26 @@
+public class methods {
+
+        //Basic Syntax:to write methods in java:-
+//              retunType methodName(parameters){
+//                  method body
+//                   }
+//         1st example:for printing the table of 2;
+   void print2Table(){
+       for(int i=1;i<=10;i++){
+           int ans=2*i;
+           System.out.println(" ->"+ans);
+
+       }
+
+   }
+     public static void main(String[] args) {
+       methods obj=new methods();
+        obj.print2Table();
+     }
+
+
+
+
+
+
+      }
