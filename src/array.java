706adbc4e14e9226e11df2f21ea4853e6abefe91
suryam tomar <tomarsuryam11@gmail.com>
2026-09-19 20:28:42 +0530

    1st program in array

---
+++ src/array.java
@@ -0,0 +1,15 @@
+public class array {
+    public static void main(String[] args){
+//       declaration;
+        int  arr [];
+//        allocation
+        arr = new int [10];
+//        initialization
+        int brr[]={100,209,390,567,678};
+        int n=brr.length;
+        for(int i=0;i<=n-1;i++){
+            System.out.println(brr[i]);
+        }
+
+    }
+}
