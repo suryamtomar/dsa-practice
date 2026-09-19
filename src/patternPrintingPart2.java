d5c6bf9de515713a9802a30069901393593d8e26
suryam tomar <tomarsuryam11@gmail.com>
2026-09-19 16:04:48 +0530

    "Pattern-4: Solid Right-Angle Triangle Pattern But With Numbers"

--- src/patternPrintingPart2.java
+++ src/patternPrintingPart2.java
@@ -59,38 +59,46 @@ public class patternPrintingPart2 {
 //
 //        }
         //Pattern-3:Butterfly-Style Star Shape Pattern:-
-             int n =5;
-           for(int row=1;row<=n;row++) {
-               //part 1:
-               for (int col = 1; col <= row; col++) {
-                   System.out.print("* ");
+//             int n =5;
+//           for(int row=1;row<=n;row++) {
+//               //part 1:
+//               for (int col = 1; col <= row; col++) {
+//                   System.out.print("* ");
+//               }
+//               //part 2:
+//               for (int col = 1; col <= 2 * (n - row); col++) {
+//                   System.out.print("  ");
+//               }
+//               //part 3:
+//               for (int col = 1; col <= row; col++) {
+//                   System.out.print("* ");
+//
+//               }
+//               System.out.println();
+//           }
+//        for(int row=1;row<=n;row++){
+//               //part 4:
+//               for(int col=1;col<=n-row+1;col++){
+//                   System.out.print("* ");
+//               }
+//               //part 5:
+//               for (int col = 1; col <=2*(row-1); col++) {
+//                   System.out.print("  ");
+//               }
+//               //part 6:
+//            for (int col = 1; col <= n-row+1; col++) {
+//                System.out.print("* ");
+//            }
+//               System.out.println();
+//           }
+        //Pattern-4: Solid Right-Angle Triangle Pattern But With Numbers:-
+               int n =5;
+               for(int row=1;row<=n;row++){
+                   for(int col=1;col<=row;col++){
+                       System.out.print(col);
+                   }
+                   System.out.println();
                }
-               //part 2:
-               for (int col = 1; col <= 2 * (n - row); col++) {
-                   System.out.print("  ");
-               }
-               //part 3:
-               for (int col = 1; col <= row; col++) {
-                   System.out.print("* ");
-
-               }
-               System.out.println();
-           }
-        for(int row=1;row<=n;row++){
-               //part 4:
-               for(int col=1;col<=n-row+1;col++){
-                   System.out.print("* ");
-               }
-               //part 5:
-               for (int col = 1; col <=2*(row-1); col++) {
-                   System.out.print("  ");
-               }
-               //part 6:
-            for (int col = 1; col <= n-row+1; col++) {
-                System.out.print("* ");
-            }
-               System.out.println();
-           }
 
 
 
@@ -105,7 +113,6 @@ public class patternPrintingPart2 {
 
 
 
-
-      }
+    }
     }
 
