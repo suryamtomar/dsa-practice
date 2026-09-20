0be5bc2eae882fed12abc12bbb0c26f99eca2b89
suryam tomar <tomarsuryam11@gmail.com>
2026-09-20 23:25:59 +0530

    "Printing the value of array using For loop and For-each loop"

--- src/array.java
+++ src/array.java
@@ -1,19 +1,20 @@
 public class array {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //       declaration;
 //        int  arr [];
 //        allocation
 //        arr = new int [10];
 //        initialization
-        int brr[]={100,209,390,567,678};
-        int n=brr.length;
+        int[] brr = {100, 209, 390, 567, 678};
+//        int n=brr.length;
 //     using for loop
         //        for(int i=0;i<=n-1;i++){
 //            System.out.println(brr[i]);
 //        }
 //     using for-each loop
-        for(int val:brr)
-        System.out.println(val);
+        for (int val : brr) {
+            System.out.println(val);
 
+        }
     }
 }
