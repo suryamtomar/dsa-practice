35f74cd337dc5f8293092534a9068927cd605441
suryam tomar <tomarsuryam11@gmail.com>
2026-09-22 16:22:00 +0530

    "#Example 3:Taking input in 2D :-"

--- src/array.java
+++ src/array.java
@@ -140,7 +140,7 @@ public class array {
 //        }
 
 //        Example 3:Taking input in 2D Array:-
-        int arr[][]=new int[3][4];
+        int[][] arr=new int[3][4];
         Scanner sc=new Scanner(System.in);
         for(int i=0;i<=arr.length-1;i++){
             for(int j=0;j<=arr[i].length-1;j++){
