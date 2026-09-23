56ae05d8a5dfaf27e1688f2fcbe29f8434c988a3
suryam tomar <tomarsuryam11@gmail.com>
2026-09-23 18:02:57 +0530

    #Basics operations in string :-

--- src/string.java
+++ src/string.java
@@ -2,7 +2,14 @@ public class string {
     public static void main(String[] args) {
         String firstName="SURYAM";
         String lastName=new String("TOMAR");
+//        Basic operation in string:-
         System.out.println(firstName+" "+lastName);
+//        It compares both First and Last name:-
+        System.out.println(firstName.compareTo(lastName));
+//        It is used for printing the length of string:-
+        System.out.println(firstName.length());
+//        It is use for printing the single character of any  string:-
+        System.out.println(firstName.charAt(2));
     }
 
 }
