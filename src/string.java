d5f70884955a4d4cb05d90d701b94240f0dd86b7
suryam tomar <tomarsuryam11@gmail.com>
2026-09-23 18:22:03 +0530

    #Taking input in string:-

--- src/string.java
+++ src/string.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 public class string {
     public static void main(String[] args) {
-        String firstName="SURYAM";
-        String lastName=new String("TOMAR");
-//        Basic operation in string:-
-        System.out.println(firstName+" "+lastName);
-//        It compares both First and Last name:-
-        System.out.println(firstName.compareTo(lastName));
-//        It is used for printing the length of string:-
-        System.out.println(firstName.length());
-//        It is use for printing the single character of any  string:-
-        System.out.println(firstName.charAt(2));
+//        String firstName="SURYAM";
+//        String lastName=new String("TOMAR");
+////        Basic operation in string:-
+//        System.out.println(firstName+" "+lastName);
+////        It compares both First and Last name:-
+//        System.out.println(firstName.compareTo(lastName));
+////        It is used for printing the length of string:-
+//        System.out.println(firstName.length());
+////        It is use for printing the single character of any  string:-
+//        System.out.println(firstName.charAt(2));
+
+//        Taking input in string:-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        System.out.println("value: "+str);
+
+
+
     }
 
 }
