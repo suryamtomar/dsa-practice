3ebaabed560b30f528d8a29e19521d01d176f72f
suryam tomar <tomarsuryam11@gmail.com>
2026-09-23 17:54:15 +0530

    #First program in string:-

---
+++ src/string.java
@@ -0,0 +1,11 @@
+public class string {
+    public static void main(String[] args) {
+        String firstName="SURYAM";
+        String lastName=new String("TOMAR");
+        System.out.println(firstName+" "+lastName);
+    }
+
+}
+
+
+
